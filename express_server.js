f51fdd3cfb087eba906749c3357446fd05ad1558
JackyLin1 <jlin9702@gmail.com>
2022-01-26 09:46:26 +0900

    added random generator for to shorten URL then redirect to /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
 
-function generateRandomString() {
+const generateRandomString = () => {
   return Math.random().toString(36).slice(7)
 }
 
@@ -40,12 +40,17 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-})
-
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL
+  };
+  res.redirect(`/urls/${shortURL}`)
+  console.log(req.body);  // Log the POST request body to the console
+})
+
